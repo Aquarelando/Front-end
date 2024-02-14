34fa9774617d4fc749910ba2644f173adb57d8df
littlledan <daniel799.dl@gmail.com>
2024-02-14 15:05:40 -0300

    Dan

--- src/paginas/cadastro/Cadastro.tsx
+++ src/paginas/cadastro/Cadastro.tsx
@@ -14,7 +14,7 @@ function Cadastro() {
   const [usuario, setUsuario] = useState<Usuario>({
     id: 0,
     nome: '',
-    tipo:'',
+    tipo:'user',
     data_nascimento: '',
     cpf: '',
     foto: '',
@@ -85,19 +85,111 @@ function Cadastro() {
     
     <h3 className="mb-10 text-3xl font-bold">Registre-se</h3>
 
-    <form className="grid gap-10 sm:grid-cols-2">
-      
-      <input type="text" name="primeiroNome" id="primeiroNome" className="py-2 pl-2 border-b border-gray-300" placeholder="Nome" />
-      <input type="text" name="sobrenome" id="sobrenome" className="py-2 pl-2 border-b border-gray-300" placeholder="Sobrenome" />
-      <input type="date" name="data_nascimento" id="data_nascimento" className="py-2 pl-2 border-b border-gray-300" placeholder="Data de Nascimento" />
-      <input type="text" name="cpf" id="cpf" className="py-2 pl-2 border-b border-gray-300" placeholder="Digite o seu CPF" required min={11} />
-      <input type="text" name="email" id="email" className="py-2 pl-2 border-b border-gray-300" placeholder="foto" />
-      <input type="email" name="email" id="email" className="py-2 pl-2 border-b border-gray-300" placeholder="Email" />
-
-      <input type="password" name="senha" id="senha" className="py-2 pl-2 border-b border-gray-300" placeholder="Senha" />
-      <input type="password" name="confirmar_senha" id="confirmar_senha" className="py-2 pl-2 border-b border-gray-300" placeholder="Confirmar Senha" />
-
-      <button type='submit' className="py-2 mt-5 text-lg font-semibold text-white bg-teal-600 sm:col-span-2 sm:w-1/2 hover:bg-teal-700">Cadastrar</button>
+    <form className="grid gap-10 sm:grid-cols-2" onSubmit={cadastrarNovoUsuario}>
+          <div className="flex flex-col w-full">
+            <label htmlFor="nome" className='text-white'>Nome</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.nome} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="tipo" className='text-white'>Tipo</label>
+            <input
+              type="text"
+              id="tipo"
+              name="tipo"
+              placeholder="Tipo"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.tipo} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="data_nascimento" className='text-white'>Data de Nascimento</label>
+            <input
+              type="date"
+              id="data_nascimento"
+              name="data_nascimento"
+              placeholder="Data de Nascimento"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.data_nascimento} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="cpf" className='text-white'>CPF</label>
+            <input
+              type="text"
+              id="cpf"
+              name="cpf"
+              placeholder="CPF(Somente em numeros"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.cpf} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="foto" className='text-white'>Foto</label>
+            <input
+              type="text"
+              id="foto"
+              name="foto"
+              placeholder="Foto"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="email" className='text-white'>Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.email} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="senha" className='text-white'>Senha</label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="confirmarSenha" className='text-white'>Confirmar Senha</label>
+            <input
+              type="password"
+              id="confirmarSenha"
+              name="confirmarSenha"
+              placeholder="Confirmar Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={confirmaSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+            />
+          </div>
+          <div className="flex justify-around w-full gap-8">
+            <button className='rounded text-white bg-[#000080] hover:bg-[#483D8B] w-1/2 py-2' onClick={back}>
+              Cancelar
+            </button>
+            <button className='rounded bg-[#000000] hover:bg-[#696969] text-white w-1/2 py-2' type='submit'>
+              Cadastrar
+            </button>
+          </div>
     </form>
   </div>
 
