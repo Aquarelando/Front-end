39c1cbd7ac85ed9931863f788e802d3690883c3b
wesley berto <wesleyberto4@gmail.com>
2024-03-14 16:30:46 -0300

    adicionando Estilo de editarProduto e DeletarProduto

--- src/components/produto/deletarProduto/DeletarProduto.tsx
+++ src/components/produto/deletarProduto/DeletarProduto.tsx
@@ -64,43 +64,39 @@ function DeletarProduto() {
     retornar();
   }
   return (
-    <div className="container w-1/3 mx-auto">
+    <div className="container mx-auto ">
       <h1 className="my-4 text-4xl text-center">Deletar Produto</h1>
 
-      <p className="mb-4 font-semibold text-center">
+      <p className="mb-4 font-semibold text-center ">
         Você tem certeza de que deseja apagar o Produto a seguir?
       </p>
 
-      <div className="flex flex-col justify-between overflow-hidden border rounded-2xl">
-        <header className="px-6 py-2 text-2xl font-bold text-white bg-emerald-500">
-          Produto
-        </header>
-        <div className="p-4">
-          <p className="text-xl h-full text-center">{produtos.nome}</p>
-          <img src={produtos.foto} />
-          <p className="text-xl h-full p-3">Descrição: {produtos.descricao}</p>
-          <div className="flex">
-            <p className="text-xl h-full p-3">
-              Validade: {produtos.dataValidade}
-            </p>
-            <p className="text-xl h-full p-3">
-              Quantidade: {produtos.quantidade}
-            </p>
+      <div className="w-full sm:w-1/3 mx-auto bg-green shadow-md hover:shadow-xl mb-6">
+        <div className="border rounded-2xl">
+          <header className="px-6 py-2 text-2xl font-bold text-white bg-emerald-500">
+            Produto
+          </header>
+          <div className="p-4">
+            <p className="text-xl text-center">{produtos.nome}</p>
+            <img src={produtos.foto} alt={produtos.nome} className="mx-auto mt-4" />
+            <p className="text-xl text-center p-3">Descrição: {produtos.descricao}</p>
+            <p className="text-xl text-center p-3">Validade: {produtos.dataValidade}</p>
+            <p className="text-xl text-center p-3">Quantidade: {produtos.quantidade}</p>
           </div>
-        </div>
-        <div className="flex">
-          <button
-            className="w-full py-2 bg-red-400 text-slate-100 hover:bg-red-600"
-            onClick={retornar}
-          >
-            Não
-          </button>
-          <button
-            className="flex items-center justify-center w-full bg-indigo-400 text-slate-100 hover:bg-indigo-600"
-            onClick={deletarProduto}
-          >
-            Sim
-          </button>
+          <div className="flex flex-col sm:flex-row">
+            <button
+              className="w-full sm:w-1/2 py-2 bg-red-400 text-slate-100 hover:bg-red-600"
+              onClick={retornar}
+            >
+              Não
+            </button>
+            <button
+              className="w-full sm:w-1/2 mt-2 sm:mt-0 py-2 bg-indigo-400 text-slate-100 hover:bg-indigo-600"
+              onClick={deletarProduto}
+            >
+              Sim
+            </button>
+          </div>
         </div>
       </div>
     </div>
